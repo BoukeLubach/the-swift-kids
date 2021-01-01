e4ee8ac96450d9a66e53b5a3a9617dfb2033c400
BoukeLubach <38408232+BoukeLubach@users.noreply.github.com>
2021-01-01 22:12:17 +0100

    updated logo3

--- tsk/settings.py
+++ tsk/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'the-swift-kids.herokuapp.com']
 
